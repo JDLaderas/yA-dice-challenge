c369ef2970c7f9d20aa1f256cf5deabd13bbe34f
JDLaderas <johnladeras21@gmail.com>
2023-04-13 17:59:22 +0400

    adding some emoji

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.querySelector(".img2").setAttribute("src", randomImageSource2);
 if (randomNumber1 > randomNumber2) {
 	document.querySelector("h1").innerHTML = "🤜️ Player 1 Wins";
 } else if (randomNumber1 < randomNumber2) {
-	document.querySelector("h1").innerHTML = "🤛️ Player 2 Wins";
+	document.querySelector("h1").innerHTML = "Player 2 Wins 🤛️";
 } else {
-	document.querySelector("h1").innerHTML = "Draw";
+	document.querySelector("h1").innerHTML = "🙅️ Draw 🙅️";
 }
