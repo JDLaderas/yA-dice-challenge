78a84a1ceaa4e384544d5ef34968df3310a1302a
JDLaderas <johnladeras21@gmail.com>
2023-04-13 17:48:54 +0400

    done doing the random number

---
+++ script.js
@@ -0,0 +1,26 @@
+// DICE 1
+
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage1 = `dice${randomNumber1}.png`;
+
+let randomImageSource1 = `./images/${randomDiceImage1}`;
+
+document.querySelector(".img1").setAttribute("src", randomImageSource1);
+
+// DICE 2
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage2 = `dice${randomNumber2}.png`;
+
+let randomImageSource2 = `./images/${randomDiceImage2}`;
+document.querySelector(".img2").setAttribute("src", randomImageSource2);
+
+// CONDITIONS
+
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "<em>Player 1 Wins</em>";
+} else if (randomNumber1 < randomNumber2) {
+	document.querySelector("h1").innerHTML = "<em>Player 2 Wins</em>"";
+} else {
+	document.querySelector("h1").innerHTML = "Draws";
+}
